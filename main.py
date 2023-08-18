72401c9259d9e0d1d10232d0fe5fb974aff78c89
Manuel Matheus <manuelmatheus.arg@gmail.com>
2023-08-17 21:07:33 -0300

    add: Full login/out

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from credentials import EMAIL, PASSWORD
 import time
 import os
 
@@ -19,15 +20,20 @@ try:
     wait = WebDriverWait(driver, 10)
     
     email_xpath = "//input[@name='mail']"
-    email_input = wait.until(EC.element_to_be_clickable((By.XPATH, email_xpath)))
+    email_input = driver.find_element(By.XPATH, email_xpath)
 
     password_xpath = "//input[@name='p']"
     password_input = driver.find_element(By.XPATH, password_xpath)
     
     driver.execute_script("arguments[0].scrollIntoView();", email_input)
     
-    email_input.send_keys("your_email@example.com")
-    password_input.send_keys("your_password")
+    email_input.send_keys(EMAIL)
+    password_input.send_keys(PASSWORD)
+
+    login_xpath = "//input[@name='s']"
+    login_button = driver.find_element(By.XPATH, login_xpath)
+    login_button.click()
+    
     # Add additional actions here, like logging in
 
    
@@ -35,6 +41,22 @@ try:
 except Exception as e:
     print("Error:", type(e).__name__, str(e))
 
+
+
 finally:
+
+    print("Logout?")
+    input_cmd = input("y/n: ")
+    if input_cmd == 'y':
+        header_setups_xpath = "//div[@id='header_setups']"
+        header_setups = driver.find_element(By.XPATH, header_setups_xpath)
+        header_setups.click()
+
+        time.sleep(5)
+
+        logout_button_xpath = "//div[@class='button_green float_left setups_logout' and text()='Logout']"
+        logout_button = driver.find_element(By.XPATH, logout_button_xpath)
+        logout_button.click()
     # Add a delay before quitting the driver
     time.sleep(15)  
+    driver.quit()
